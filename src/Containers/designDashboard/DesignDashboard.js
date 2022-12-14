8ddece01cac5074e6b6a9b5e2bd348f0b1000874
Barcha1 <abbarcha123456@gmail.com>
2022-12-14 19:33:01 +0500

    new chnages

--- src/Containers/designDashboard/DesignDashboard.js
+++ src/Containers/designDashboard/DesignDashboard.js
@@ -5,6 +5,7 @@ import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
+import { Grid } from "@mui/material"
 
 
 import BathtubIcon from '@material-ui/icons/Bathtub';
@@ -26,41 +27,6 @@ import Navs from '../../components/navbar/Nav';
 
 
 
-const drawerWidth = 235;
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `vertical-tab-${index}`,
-    'aria-controls': `vertical-tabpanel-${index}`,
-  };
-}
-
-
 
 
 export default function DesignDashborad() {
@@ -75,97 +41,83 @@ export default function DesignDashborad() {
   return (
     <div className='designDashboard'>
       <Navs />
-      <Box sx={{ display: 'flex' }}>
-        <Drawer
-          sx={{
-            width: drawerWidth,
-            flexShrink: 0,
-            '& .MuiDrawer-paper': {
-              width: drawerWidth,
-              boxSizing: 'border-box',
-            },
-          }}
-          variant="permanent"
-          anchor="left"
+      < Grid container >
+
+        <Grid item lg={2.5} md={3} sm={3}
+          className="leftSidebar"
         >
-          <List>
-            <Box
-              className="leftSidebar"
-              sx={{ flexGrow: 1, display: 'flex' }}
-            >
-              <div className='sidbarIcons'>
-                <Link to="Tabl" onClick={()=>setElemt(true)}>
-                  <button>
-                    <TableChartIcon />
-                  </button>
-                </Link>
-                <Link to="Tab2">
-                  <button>
-                    <AccountTreeIcon />
-                  </button>
-                </Link>
-                <Link to="Tab3">
-                  <button>
-                    <NatureIcon />
-                  </button>
-                </Link>
-                <Link to="Tab4">
+          <Grid container>
+            <Grid item lg={1.8} md={2} sm={2} xs={12} className='sidbarIcons'>
+              <Link to="Tabl" onClick={() => setElemt(true)}>
+                <TableChartIcon />
+              </Link>
+              <Link to="Tab2">
 
-                  <button>
-                    <WeekendIcon />
-                  </button>
-                </Link>
+                <AccountTreeIcon />
 
-                <Link to="Tab5">
-                  <button>
-                    <BathtubIcon />
-                  </button>
-                </Link>
-                <Link to="Tab6">
-                  <button>
-                    <MeetingRoomIcon />
-                  </button>
-                </Link>
-                <Link to="Tab7">
-                  <button>
-                    <LocalHotelIcon />
-                  </button>
-                </Link>
-                <Link to="Tab8">
-                  <button>
-                    <LocalHospitalIcon />
-                  </button>
-                </Link>
-              </div>
-              <div className='Sidebar-data'>
-                <Routes>
-                  <Route path="/Tabl" element={<Tables title="Drawers" />} />
-                  <Route path="/Tab2" element={<Tables title="Kitchen Assets" />} />
-                  <Route path="/Tab3" element={<Tables title="Tables" />} />
-                  <Route path="/Tab4" element={<Tables title="Chairs" />} />
-                  <Route path="/Tab5" element={<Tables title="Bathroom Assets" />} />
-                  <Route path="/Tab6" element={<Tables title="Devices and Electronics" />} />
-                  <Route path="/Tab7" element={<Tables title="Bedroom Assets" />} />
-                  <Route path="/Tab8" element={<Tables title="2" />} />
-                </Routes>
-              </div>
+              </Link>
+              <Link to="Tab3">
 
-            </Box>
-          </List>
+                <NatureIcon />
 
-        </Drawer>
-        <Box
+              </Link>
+              <Link to="Tab4">
+
+
+                <WeekendIcon />
+
+              </Link>
+
+              <Link to="Tab5">
+
+                <BathtubIcon />
+
+              </Link>
+              <Link to="Tab6">
+
+                <MeetingRoomIcon />
+
+              </Link>
+              <Link to="Tab7">
+
+                <LocalHotelIcon />
+
+              </Link>
+              <Link to="Tab8">
+
+                <LocalHospitalIcon />
+
+              </Link>
+            </Grid>
+            <Grid item lg={10} md={10} sm={3} xs={12} className='Sidebar-data'>
+              <Routes>
+                <Route path="/Tabl" element={<Tables title="Drawers" />} />
+                <Route path="/Tab2" element={<Tables title="Kitchen Assets" />} />
+                <Route path="/Tab3" element={<Tables title="Tables" />} />
+                <Route path="/Tab4" element={<Tables title="Chairs" />} />
+                <Route path="/Tab5" element={<Tables title="Bathroom Assets" />} />
+                <Route path="/Tab6" element={<Tables title="Devices and Electronics" />} />
+                <Route path="/Tab7" element={<Tables title="Bedroom Assets" />} />
+                <Route path="/Tab8" element={<Tables title="2" />} />
+              </Routes>
+            </Grid>
+          </Grid>
+
+        </Grid>
+
+
+        <Grid lg={7} md={6} sm={8} xs={12}
           component="main"
-          sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, borderTop: "1px solid black" }}
+          className='middlesection'
         >
-          {/* center content */}
-        </Box>
-        <List>
-          <Box className="rightbar">
-            <Rightbar />
-          </Box>
-        </List>
-      </Box>
+          sdcsdfsdfsd
+        </Grid>
+
+        <Grid className="rightbar" lg={2.5} md={3} sm={12} xs={12}>
+          <Rightbar />
+        </Grid>
+
+      </Grid>
     </div>
   );
 }
